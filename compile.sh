7fdf92e46bb23ef033c75d8c77e659b040099816
Jakub Tomczyński <tomczynski.jakub@gmail.com>
2019-02-13 18:13:50 +0100

    compile.sh: preserve original source modification date

--- compile.sh
+++ compile.sh
@@ -18,4 +18,5 @@ find . -iname "*.md" | while read f ; do
 	title=`basename "$f"`
 	grip --export --title "${title%.*}" "$f" "../public/${f%md}html"
 	"${SED[@]}" 's/\/__\/grip\/static\//..\/static\//g' "../public/${f%md}html"
+	touch -r "$f" -c -m "../public/${f%md}html"
 done
